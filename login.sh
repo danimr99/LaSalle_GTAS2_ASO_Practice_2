6324445d553581bfa5c6e1e63a097b182c5ebdf5
Daniel Muelle <danimr1999@gmail.com>
2022-01-03 03:19:03 +0100

    Initial commit

---
+++ login.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+echo "
+    <!doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta name="description" content="">
+            <title>Web Admin</title>
+
+            <!-- Bootstrap core CSS -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+                integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+            <style>
+            .bd-placeholder-img {
+                font-size: 1.125rem;
+                text-anchor: middle;
+                -webkit-user-select: none;
+                -moz-user-select: none;
+                user-select: none;
+            }
+
+            @media (min-width: 768px) {
+                .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+                }
+            }
+            </style>
+            
+            <!-- Custom styles for this template -->
+            <link href="./css/sign_in.css" rel="stylesheet">
+        </head>
+        <body class="text-center">
+            <main class="form-signin">
+                <form action="home.sh" method="POST">
+                    <img class="mb-4" src="./images/configuration.png" alt="" width="75" height="75">
+                    <h1 class="h3 fw-normal">Sign in</h1>
+
+                    <div class="form-floating mb-3">
+                        <input id="username" type="text" class="form-control" id="floatingInput" placeholder="Username">
+                    </div>
+                    <div class="form-floating mb-3">
+                        <input id="password" type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                    </div>
+
+                    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                </form>
+            </main>   
+            
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" 
+                integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" 
+                integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" 
+                integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        </body>
+    </html>
+"
+
+
